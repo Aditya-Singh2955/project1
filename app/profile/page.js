2aff8544a46ba7dafb1d140e3c0bd500eb2f62ea
ADITYA <91845934+Aditya-Singh2955@users.noreply.github.com>
2025-02-23 01:58:54 +0530

    Update page.js

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const profile = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full text-3xl flex-col">
-    <p>I have received your message. I will respond to you shortly.</p>
+    <p>I have received your message. I will respond to you shorty.</p>
     <br/><p>Thank you For Visiting My PortFolio.</p><br/>
     <p>Have a Nice day.</p>
     </div>
